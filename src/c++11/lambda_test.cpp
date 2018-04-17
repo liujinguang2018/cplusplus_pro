6c8baf1c14aa0f76b9e9e7990e914557f55bccbd
liujinguang2018 <jinguang.liu@gmail.com>
2018-04-17 23:13:49 +0800

    add lambda example

---
+++ src/c++11/lambda_test.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#
+
+using namespace std;
+
+int main()
+{
+    auto f1 = [](int x) { return x * x; }; //lambda表达式的类型为闭包(closure type), 无法直接给出
+    auto f2 = [](string s) { cout << "lambda: " << s << endl; };
+    auto f3 = [](int x, int y)->bool { return x > y; };
+
+    cout << f1(3) << endl;
+    f2("heavy rain");
+    cout << f3(3, 5) << endl;
+
+    vector<int> v = {1, 2, 3, 5};
+    for_each(v.begin(), v.end(), [](int &x) { if (x > 3) x *= 3; });
+    //copy(v.begin(), v.end(), ostream_iterator<int>(cout, ","));
+    for_each(v.begin(), v.end(), [](int &x) { cout << x << ","; });
+
+}
---
+++ src/c++11/move_test.cpp
@@ -0,0 +1,17 @@
+﻿#include <utility>
+#include <algorithm>
+
+class movable
+{
+  private:
+    int x;
+
+  public:
+    movable() {}
+    movable(movable &&other) //转移构造函数
+    {
+        std::swap(x, other.x);
+    }
+
+    //movable&
+};
